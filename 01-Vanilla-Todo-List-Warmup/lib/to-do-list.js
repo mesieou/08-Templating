97bbcb7de5ad6a0e77a6eebf05dafec094db9266
juan bernal <mesieou@gmail.com>
2024-01-08 16:39:26 +1100

    todo list completed

--- 01-Vanilla-Todo-List-Warmup/lib/to-do-list.js
+++ 01-Vanilla-Todo-List-Warmup/lib/to-do-list.js
@@ -5,10 +5,13 @@ const todos = [
   { title: "Water the plants", done: true }
 ];
 
-// To-do item HTML 👇
-// <div class="shadow-sm rounded px-4 py-3 mb-2 border d-flex">
-//   <input class="d-flex form-check-input me-1" type="checkbox">
-//   <div>Replace with to-do title</div>
-// </div>
+const todoContainer = document.querySelector('#todosContainer');
 
 // TODO: Dynamically generate a list of to-dos based on `todos` array, and display them
+todos.forEach(todo => {
+  const result = `<div class="shadow-sm rounded px-4 py-3 mb-2 border d-flex">
+    <input class="d-flex form-check-input me-1" type="checkbox"${ todo.done ? 'checked' : '' }>
+    <div>${todo.title}</div>
+  </div>`
+  todoContainer.insertAdjacentHTML('beforeend', result)
+});
