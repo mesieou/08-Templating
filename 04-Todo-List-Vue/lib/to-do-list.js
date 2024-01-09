d2ebfc7296e024f111a3f971814c35e13185495c
juan bernal <mesieou@gmail.com>
2024-01-09 16:03:17 +1100

    see my todo list with vue js

--- 04-Todo-List-Vue/lib/to-do-list.js
+++ 04-Todo-List-Vue/lib/to-do-list.js
@@ -1,9 +1,16 @@
 import { createApp } from "vue";
 
+const todos = [
+  { title: "Code a to-do list", done: false },
+  { title: "Eat breakfast", done: true },
+  { title: "Do some exercise", done: false },
+  { title: "Water the plants", done: true }
+];
+
 createApp({
   data() {
     return {
-      message: "If you see this message in your browser, that means Vue is successfully mounted! 🙌"
+      todos: todos
     }
   }
-}).mount('#todosContainer');
+}).mount('#app');
