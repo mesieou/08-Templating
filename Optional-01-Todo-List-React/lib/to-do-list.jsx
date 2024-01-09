2ea36d2d7d37827e3e67d0a8abe5c94e84b884de
juan bernal <mesieou@gmail.com>
2024-01-10 10:44:26 +1100

    react displaying task

--- Optional-01-Todo-List-React/lib/to-do-list.jsx
+++ Optional-01-Todo-List-React/lib/to-do-list.jsx
@@ -1,9 +1,24 @@
 const App = () => {
-  const message = "If you see this message in your browser, that means React is successfully mounted! 🙌";
+  const [todos, changeTodos] = React.useState(
+    [
+      { title: "Code a to-do list", done: false },
+      { title: "Eat breakfast", done: true },
+      { title: "Do some exercise", done: false },
+      { title: "Water the plants", done: true }
+    ]
+  )
 
   return (
-    <div id="todosContainer">
-      {message}
+    <div id="app">
+    {todos.map((todo, index) => (
+      <div class="shadow-sm rounded px-4 py-3 mb-2 border d-flex">
+        <input class="d-flex form-chech-input me-1" type="checkbox"/>
+        <div class="d-flex flex-grow-1">
+            <div key={index}>{todo.title}</div>
+            <div class="ms-auto">✖️</div>
+        </div>
+      </div>
+    ))}
     </div>
   );
 }
